de4b731f157483e8796e10e20b39d2b524a35994
Gruetzig <88926852+Gruetzig@users.noreply.github.com>
2022-08-06 17:14:45 +0200

    Update miiminescript.bat
    fix the entire script after finding out im dumb

--- miiminescript.bat
+++ miiminescript.bat
@@ -1,12 +1,9 @@
 @echo off
 echo Mii Mine Script! & echo Before Running this Script, make sure you have input.bin in this directory as well as Python 3 installed
 
-set /p id0=Enter your ID0:
-
 
 py -3 -m pip install pycryptodomex
 
-py -3 seedminer_launcher3.py id0 %id%
 
 set /p model=Is This and Old or New Model(say new or old):
 
@@ -14,6 +11,12 @@ set /p year=Do you know when the (N)3DS/2DS(XL/LL) was built(2011-2020)(leave bl
 
 py -3 seedminer_launcher3.py mii %model% %year%
 
+set /p id0=Enter your ID0:
+
+py -3 seedminer_launcher3.py id0 %id%
+
 echo movable outputted to this directory or unminable console
+
+py -3 seedminer_launcher3.py gpu
 
 pause
